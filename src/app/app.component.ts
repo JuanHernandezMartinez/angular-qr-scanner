c3e1e7c63e133c4200a8d572d2b6f4fcf3d9959d
Administrator <juan.martinez3168@gmail.com>
2024-10-09 11:19:15 -0600

    QR terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, ViewChild } from '@angular/core';
+import { NgxScannerQrcodeModule, LOAD_WASM } from 'ngx-scanner-qrcode';
+
+LOAD_WASM().subscribe();
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [NgxScannerQrcodeModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular-qr-scanner';
+  @ViewChild('action') action: any;
+
+  obtener(event: any) {
+    this.action.stop();
+    console.log(event[0].value);
+    window.open(event[0].value, 'blank');
+  }
 }
